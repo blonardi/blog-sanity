dcb2b8fa98d1a9b4f55413e63d9df2ca001fe5da
blonardi <lonardibautista@gmail.com>
2024-06-13 16:17:25 -0300

    Implement new schemas

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -2,7 +2,9 @@ import { type SchemaTypeDefinition } from 'sanity'
 import { post } from './schemas/post'
 import { author } from './schemas/author'
 import { tag } from './schemas/tag'
+import { house } from './schemas/house'
+import { owner } from './schemas/owner'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, tag]
+  types: [post, author, house, owner, tag]
 }
