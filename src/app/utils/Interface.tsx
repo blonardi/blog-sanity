94d5c66bf41ee6ff402e7b064640f24c6689952b
blonardi <lonardibautista@gmail.com>
2024-06-13 16:18:09 -0300

    Add new interfaces and add new fields

--- src/app/utils/Interface.tsx
+++ src/app/utils/Interface.tsx
@@ -9,8 +9,59 @@ export interface Post{
 }
 
 export interface Tag{
+	_id: string,
 	name: string,
 	slug: {current: string},
-	_id: string,
-	postCount?: number
+	postCount?: number,
+	contentCount?: number,
+}
+
+// Definición de los tipos para las referencias
+interface Reference {
+  _type: 'reference';
+  _ref: string;
+}
+
+// Definición de los tipos para las imágenes
+interface Image {
+  _type: 'image';
+  asset: Reference;
+  alt?: string;
+}
+
+// Definición del tipo para los bloques de contenido
+interface Block {
+  _type: 'block' | 'image';
+  children?: Array<{ text: string }>; // Ajustar según la estructura de los bloques de texto
+  asset?: Reference;
+  alt?: string;
+}
+
+
+// Definición de los tipos para los dueños
+export interface Owner {
+  _id: string;
+  name: string;
+	slug: {current: string},
+	contact:string;
+	houseCount?: number,
+}
+
+export interface House {
+	_id: string;
+	_type: 'house';
+  title: string;
+  slug: {
+    current: string;
+    _type: 'slug';
+  };
+  description: string;
+  excerpt: string;
+  coverImage: Image;
+  gallery: Image[];
+  publishedAt: string;
+  body: any;
+  capacity: number;
+  owner: Owner; // Referencia a un documento de tipo 'owner'
+  tags: Array<Tag>; // Referencias a documentos de tipo 'tag'
 }
